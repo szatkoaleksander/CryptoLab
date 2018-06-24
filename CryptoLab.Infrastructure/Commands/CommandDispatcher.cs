3ebe1e8d9aa0d1056dadcf3ed6551d3bb332d2df
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-06-24 20:35:57 +0200

    Implemented command handler pattern

---
+++ CryptoLab.Infrastructure/Commands/CommandDispatcher.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace CryptoLab.Infrastructure.Commands
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        private readonly IComponentContext _context;
+
+        public CommandDispatcher(IComponentContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task DispatchAsync<T>(T command) where T : ICommand
+        {
+            if(command == null)
+            {
+                throw new Exception("Command cannot be null");
+            }
+            
+            var handler = _context.Resolve<ICommandHandler<T>>();
+            await handler.HandlerAsync(command);
+        }
+    }
+}
