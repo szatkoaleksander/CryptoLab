cd32be627c8ec794222c5409560a9111eea55284
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-06-24 16:11:02 +0200

    Implement Register and Login methods

--- CryptoLab.Infrastructure/Services/UserService.cs
+++ CryptoLab.Infrastructure/Services/UserService.cs
@@ -3,16 +3,19 @@ using System.Threading.Tasks;
 using CryptoLab.Domain.Domain;
 using CryptoLab.Domain.IRepositories;
 using CryptoLab.Infrastructure.IServices;
+using CryptoLab.Infrastructure.JWT;
 
 namespace CryptoLab.Infrastructure.Services
 {
     public class UserService : IUserService
     {
         private readonly IUserRepository _userRepository;
+        private readonly IEncrypter _encrypter;
 
-        public UserService(IUserRepository userRepository)
+        public UserService(IUserRepository userRepository, IEncrypter encrypter)
         {
             _userRepository = userRepository;
+            _encrypter = encrypter;
         }
 
         public async Task<User> FindAsync(Guid id)
@@ -33,6 +36,25 @@ namespace CryptoLab.Infrastructure.Services
         {
             var user = await _userRepository.FindAsync(email);
 
+            if(user == null)
+            {
+                throw new Exception("Invalid credentials");
+            }
+
+            var salt = _encrypter.GetSalt(password);
+            var hash = _encrypter.GetHash(password, user.Salt);
+
+            if(hash == null)
+            {
+                throw new Exception("Invalid credentials");
+            }
+
+            if(user.Password == hash)
+            {
+                return;
+            }
+
+            throw new Exception("Invalid credentials");
         }
 
         public async Task RegisterAsync(string email, string username, string password)
@@ -41,6 +63,13 @@ namespace CryptoLab.Infrastructure.Services
 
             if(user != null) 
                 throw new Exception("User is exists");
+
+            var salt = _encrypter.GetSalt(password);
+            var hash = _encrypter.GetHash(password, salt);
+
+            user = new User(email, username, hash, salt, "user");
+
+            await _userRepository.AddAsync(user);
         }
 
         public async Task RemoveAccountAsync(Guid id)
