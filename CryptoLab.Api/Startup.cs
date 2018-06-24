644e50988d519df64c018a8624acc520b50ed8b5
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-06-24 20:46:52 +0200

    Added: roles, JwtBearer options, autofac builder

--- CryptoLab.Api/Startup.cs
+++ CryptoLab.Api/Startup.cs
@@ -17,19 +17,28 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using CryptoLab.Infrastructure.JWT;
+using CryptoLab.Infrastructure.IoC;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
 
 namespace CryptoLab.Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public IContainer ApplicationContainer { get; private set; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IUserService, UserService>();
@@ -37,14 +46,60 @@ namespace CryptoLab.Api
             services.AddSingleton<IEncrypter, Encrypter>();
             services.AddSingleton<IJwtHandler, JwtHandler>();
 
+
             services.AddDbContext<Context>(options =>
                 options.UseInMemoryDatabase("db"));
 
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("user", policy => policy
+                    .RequireAuthenticatedUser().RequireClaim(ClaimTypes.Role, "user")
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                );
+
+                options.AddPolicy("admin", policy => policy
+                    .RequireAuthenticatedUser().RequireClaim(ClaimTypes.Role, "admin")
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                );
+            });
+            
+            services.AddAuthentication(options =>  
+            {  
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;  
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; 
+            })  
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("000_cat_test_key_123")),  
+
+                    ValidateIssuer = true,  
+                    ValidIssuer = "http://localhost:5000",
+
+                    ValidateAudience = false,   
+                    ValidateLifetime = true, 
+                    RoleClaimType = "roles", 
+                   
+            
+                    ClockSkew = TimeSpan.Zero 
+                };
+            });
 
             services.AddMvc();
+
+            var builder = new ContainerBuilder();
+                builder.Populate(services);
+                builder.RegisterModule<CommandModule>();
+
+                ApplicationContainer = builder.Build();
+
+            return new AutofacServiceProvider(ApplicationContainer);
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLifetime)
         {
             if (env.IsDevelopment())
             {
@@ -56,6 +111,9 @@ namespace CryptoLab.Api
             }
 
             app.UseMvc();
+            app.UseAuthentication();
+
+            appLifetime.ApplicationStopped.Register(() => ApplicationContainer.Dispose());
         }
     }
 }
