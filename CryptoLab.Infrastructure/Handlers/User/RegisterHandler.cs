0c2911d9d387e5c1ec1436a60d102e7fb9c876f0
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-06-24 20:43:23 +0200

    Added Register handler

---
+++ CryptoLab.Infrastructure/Handlers/User/RegisterHandler.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using CryptoLab.Infrastructure.Commands;
+using CryptoLab.Infrastructure.Commands.User;
+using CryptoLab.Infrastructure.IServices;
+
+namespace CryptoLab.Infrastructure.Handlers.User
+{
+    public class RegisterHandler : ICommandHandler<Register>
+    {
+        private readonly IUserService _userService;
+
+        public RegisterHandler(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task HandlerAsync(Register command)
+        {
+            await _userService.RegisterAsync(command.Email, command.Username, command.Password);
+        }
+    }
+}
