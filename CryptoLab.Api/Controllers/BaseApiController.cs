e8966777bdbaafa6cf6ef78704ce133ee5a36b18
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-06-25 17:58:20 +0200

    Added BaseApiController

--- CryptoLab.Api/Controllers/BaseApiController.cs
+++ CryptoLab.Api/Controllers/BaseApiController.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Threading.Tasks;
+using CryptoLab.Infrastructure.Commands;
+using CryptoLab.Infrastructure.Commands.Auth;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CryptoLab.Api.Controllers
@@ -5,6 +9,25 @@ namespace CryptoLab.Api.Controllers
     [Route("api/[controller]")]
     public abstract class BaseApiController : Controller
     {
-        
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        protected Guid UserId => User?.Identity?.IsAuthenticated == true ?
+            Guid.Parse(User.Identity.Name) :
+            Guid.Empty;
+
+        protected BaseApiController(ICommandDispatcher commandDispatcher)
+        {
+            _commandDispatcher = commandDispatcher;
+        }
+
+        protected async Task DispatchAsync<T>(T command) where T : ICommand
+        {
+            if(command is IAuthCommand authCommand)
+            {
+                authCommand.UserId = UserId;
+            }
+
+            await _commandDispatcher.DispatchAsync(command);
+        }
     }
 }
