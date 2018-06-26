cbed1b5c36228f009223e03aead60726c888fff8
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-06-26 18:14:18 +0200

    Added Wallet model

---
+++ CryptoLab.Domain/Domain/Wallet.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace CryptoLab.Domain.Domain
+{
+    public class Wallet
+    {
+        public Guid Id { get; protected set; }
+        public string Currnecy { get; protected set; }
+        public double AmountOfMoney { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+        public Guid UserId { get; protected set; }
+        public User User { get; protected set; }
+        
+
+
+        public Wallet(string currency, double amountOfMoney, User user)
+        {
+            Id = Guid.NewGuid();
+            SetCurrnecy(currency);
+            SetAmountOfMoney(amountOfMoney);
+
+            UpdatedAt = DateTime.Now;
+
+            UserId = user.Id;
+            User = user;
+        }
+
+        protected Wallet() {}
+
+        public void SetCurrnecy(string currency)
+        {
+            Currnecy = currency;
+        }
+
+        public void SetAmountOfMoney(double amountOfMoney)
+        {
+            if(amountOfMoney < 0.0)
+                throw new Exception("Amount of money can not be null");
+
+            AmountOfMoney = amountOfMoney;
+        }
+    }
+}
