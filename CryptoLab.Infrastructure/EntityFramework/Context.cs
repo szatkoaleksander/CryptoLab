c057fc4f3d3a6d3abf574ef463f243e2086bc87f
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-06-26 18:24:10 +0200

    Added Wallet adn History context

--- CryptoLab.Infrastructure/EntityFramework/Context.cs
+++ CryptoLab.Infrastructure/EntityFramework/Context.cs
@@ -6,6 +6,8 @@ namespace CryptoLab.Infrastructure.EntityFramework
     public class Context : DbContext
     {
         public DbSet<User> User { get; set; }
+        public DbSet<Wallet> Wallet { get; set; }
+        public DbSet<History> History { get; set; }
 
         public Context(DbContextOptions<Context> options) : base(options)
         {
@@ -15,6 +17,16 @@ namespace CryptoLab.Infrastructure.EntityFramework
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<User>()
+                .HasMany(x => x.Wallets)
+                .WithOne(x => x.User)
+                .HasForeignKey(x => x.UserId).IsRequired().OnDelete(DeleteBehavior.SetNull);
+
+            builder.Entity<User>()
+                .HasMany(x => x.Histories)
+                .WithOne(x => x.User)
+                .HasForeignKey(x => x.UserId).IsRequired().OnDelete(DeleteBehavior.SetNull);
         }
     }
 }
