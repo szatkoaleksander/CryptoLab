e41465692b4c165ee2cd811d9ba8e22f422f2b8f
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-06-27 19:48:05 +0200

    Added missing suffix async

--- CryptoLab.Infrastructure/IServices/ITransactionService.cs
+++ CryptoLab.Infrastructure/IServices/ITransactionService.cs
@@ -6,6 +6,6 @@ namespace CryptoLab.Infrastructure.IServices
     public interface ITransactionService
     {
          Task FastBuyTransactionAsync(string toCurrnecy, double amount, Guid userId);
-         Task FastSellTransaction(string fromCurrnecy, double amount, Guid userId);
+         Task FastSellTransactionAsync(string fromCurrnecy, double amount, Guid userId);
     }
 }
