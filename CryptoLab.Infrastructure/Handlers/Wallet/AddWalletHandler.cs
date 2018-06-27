6e7d8d2c9e5e02e5f69ca2d58e63ac6a7563d021
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-06-27 16:25:28 +0200

    Added AddWallet command and handler

---
+++ CryptoLab.Infrastructure/Handlers/Wallet/AddWalletHandler.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using CryptoLab.Infrastructure.Commands;
+using CryptoLab.Infrastructure.Commands.Wallet;
+using CryptoLab.Infrastructure.IServices;
+
+namespace CryptoLab.Infrastructure.Handlers.Wallet
+{
+    public class AddWalletHandler : ICommandHandler<AddWallet>
+    {
+        private readonly IWalletService _walletService;
+
+        public AddWalletHandler(IWalletService walletService)
+        {
+            _walletService = walletService;
+        }
+
+        public async Task HandlerAsync(AddWallet command)
+        {
+            await _walletService.AddAsync(command.Currency, command.UserId);
+        }
+    }
+}
