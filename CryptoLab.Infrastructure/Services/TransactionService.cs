2105034f274c090ddb2df0c3d89512d6abd9d6cb
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-06-27 19:50:33 +0200

    Added logic to FastSellTransaction and changed logic in FastBuyTransaction
    LINQ <3

--- CryptoLab.Infrastructure/Services/TransactionService.cs
+++ CryptoLab.Infrastructure/Services/TransactionService.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using CryptoLab.Domain.IRepositories;
 using CryptoLab.Infrastructure.CryptoCompareApi;
 using CryptoLab.Infrastructure.IServices;
+using System.Linq;
 
 namespace CryptoLab.Infrastructure.Services
 {
@@ -24,27 +25,39 @@ namespace CryptoLab.Infrastructure.Services
             var user = await _userRepository.FindAsync(userId); 
             var wallet = await _walletRepository.GetByUserId(userId); 
 
-            double price = await _cryptoCompareApi.GetCryptoPriceInUsd(toCurrnecy);
+            var price = await _cryptoCompareApi.GetCryptoPriceInUsd(toCurrnecy);
 
-            foreach(var i in wallet)
-            {
-                if(i.Currnecy == "USD")
-                {
-                    i.SetAmountOfMoney(i.AmountOfMoney - amount);
-                }
+            var fromWallet = wallet.Where(x => x.Currnecy == "USD" && (x.AmountOfMoney - (amount * price)) >= 0).SingleOrDefault();
+            var toWallet = wallet.Where(x => x.Currnecy == toCurrnecy).SingleOrDefault();
 
-                if(i.Currnecy == toCurrnecy)
-                {
-                    i.SetAmountOfMoney((i.AmountOfMoney + amount) / price);
-                }
+            if(fromWallet == null || toWallet == null)
+                throw new Exception("Wallet is not exists");
 
-                await _walletRepository.UpdateAsync(i); 
-            }
+            fromWallet.SetAmountOfMoney(fromWallet.AmountOfMoney - (amount * price));
+            toWallet.SetAmountOfMoney(toWallet.AmountOfMoney + amount);
+
+            await _walletRepository.UpdateAsync(fromWallet); 
+            await _walletRepository.UpdateAsync(toWallet);
         }
 
-        public Task FastSellTransaction(string fromCurrnecy, double amount, Guid userId)
+        public async Task FastSellTransactionAsync(string fromCurrnecy, double amount, Guid userId)
         {
-            throw new NotImplementedException();
+            var user = await _userRepository.FindAsync(userId); 
+            var wallet = await _walletRepository.GetByUserId(userId); 
+
+            var price = await _cryptoCompareApi.GetCryptoPriceInUsd(fromCurrnecy);
+
+            var fromWallet = wallet.Where(x => x.Currnecy == fromCurrnecy && (x.AmountOfMoney - amount) >= 0).SingleOrDefault();
+            var toWallet = wallet.Where(x => x.Currnecy == "USD").SingleOrDefault();
+
+            if(fromWallet == null || toWallet == null)
+                throw new Exception("Wallet is not exists");
+
+            fromWallet.SetAmountOfMoney(fromWallet.AmountOfMoney - amount);
+            toWallet.SetAmountOfMoney(toWallet.AmountOfMoney + amount * price);
+
+            await _walletRepository.UpdateAsync(fromWallet); 
+            await _walletRepository.UpdateAsync(toWallet);
         }
     }
 }
