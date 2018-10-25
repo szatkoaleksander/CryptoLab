0f9d1af75ffa1a19c3bc8c3e444d60590474a054
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-10-25 12:54:17 +0200

    Updated Fast Buy/Sell Handler
    Added code which saving transaction to histories

--- CryptoLab.Infrastructure/Handlers/Transaction/FastBuyHandler.cs
+++ CryptoLab.Infrastructure/Handlers/Transaction/FastBuyHandler.cs
@@ -1,6 +1,8 @@
 using System.Threading.Tasks;
+using CryptoLab.Domain.Domain;
 using CryptoLab.Infrastructure.Commands;
 using CryptoLab.Infrastructure.Commands.Transaction;
+using CryptoLab.Infrastructure.CryptoCompareApi;
 using CryptoLab.Infrastructure.IServices;
 
 namespace CryptoLab.Infrastructure.Handlers.Transaction
@@ -8,15 +10,20 @@ namespace CryptoLab.Infrastructure.Handlers.Transaction
     public class FastBuyHandler : ICommandHandler<FastBuy>
     {
         private readonly ITransactionService _transactionService;
+        private readonly IHistoryService _historyService;
 
-        public FastBuyHandler(ITransactionService transactionService)
+        public FastBuyHandler(ITransactionService transactionService, IHistoryService historyService)
         {
             _transactionService = transactionService;
+            _historyService = historyService;
         }
 
         public async Task HandlerAsync(FastBuy command)
         {
             await _transactionService.FastBuyTransactionAsync(command.ToCurrency, command.Amount, command.UserId);
+
+            var price = await CryptoCompare.GetCryptoPriceInUsd(command.ToCurrency);
+            await _historyService.AddAsync(OperationType.Buy, command.ToCurrency, command.Amount, price, command.UserId);
         }
     }
 }
