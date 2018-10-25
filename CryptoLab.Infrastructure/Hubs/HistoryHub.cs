9130d41a6b1e9c052f1c92ff97ec36217883ec55
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-10-25 12:51:08 +0200

    Added HistoryHub

---
+++ CryptoLab.Infrastructure/Hubs/HistoryHub.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using CryptoLab.Domain.Domain;
+using CryptoLab.Infrastructure.DTO;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CryptoLab.Infrastructure.Hubs
+{
+    public class HistoryHub : Hub<IHistoryHub>
+    {
+        public async Task Add(History history) 
+            => await Clients.All.Add(history);
+    }
+}
