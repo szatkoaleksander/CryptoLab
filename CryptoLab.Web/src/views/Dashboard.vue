633353c3854e09fe6aa9b8efabf3514144c8d6fc
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-10-30 18:09:42 +0100

    Added CryptoLab.Web project

---
+++ CryptoLab.Web/src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="isAuthenticated">
+      <h1>Dashboard</h1>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters([
+      'isAuthenticated'
+    ])
+  }
+}
+</script>
