448566501f5cd58bad721c5011da9b027e8a995c
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-11-05 20:42:32 +0100

    Added Login and Register components

---
+++ CryptoLab.Web/src/components/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <b-btn v-b-modal.modal>Login</b-btn>
+      <div class="text-left">
+        <b-modal id="modal" title="Login" hide-footer>
+          <b-form v-on:submit.prevent="onSubmit(email, password)">
+            <b-form-group id="emailInputGroup"
+              label="Email address:"
+              label-for="emailInput">
+              <b-form-input id="emailInput"
+                type="email"
+                v-model="email"
+                required
+                placeholder="Enter email">
+              </b-form-input>
+            </b-form-group>
+            <b-form-group id="passwordInputGroup"
+              label="Your Password:"
+              label-for="passwordInput">
+              <b-form-input id="passwordInput"
+                type="password"
+                v-model="password"
+                required
+                placeholder="Enter password">
+              </b-form-input>
+            </b-form-group>
+
+            <b-button class="float-right" type="submit" variant="primary" data-dismiss="modal">Login</b-button>
+          </b-form>
+        </b-modal>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { LOGIN } from '@/stores/actions.type'
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      email: null,
+      password: null
+    }
+  },
+  methods: {
+    onSubmit (email, password) {
+      this.$store
+        .dispatch(LOGIN, { email, password })
+        .then(() => this.$router.push({ name: 'dashboard' }))
+    },
+  }
+}
+</script>
---
+++ CryptoLab.Web/src/components/Register.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <b-btn v-b-modal.registerModal>Register</b-btn>
+      <div class="text-left">
+        <b-modal id="registerModal" title="Register" hide-footer>
+          <b-form v-on:submit.prevent="onSubmit(email, username, password)">
+            <b-form-group id="emailInputGroup1"
+              label="Email address:"
+              label-for="emailInput1">
+              <b-form-input id="emailInput1"
+                type="email"
+                v-model="email"
+                required
+                placeholder="Enter email">
+              </b-form-input>
+            </b-form-group>
+            <b-form-group id="usernameInputGroup"
+              label="Your username:"
+              label-for="usernameInput">
+              <b-form-input id="usernameInput"
+                type="text"
+                v-model="username"
+                required
+                placeholder="Enter username">
+              </b-form-input>
+            </b-form-group>
+            <b-form-group id="passwordInputGroup1"
+              label="Your Password:"
+              label-for="passwordInput1">
+              <b-form-input id="passwordInput1"
+                type="password"
+                v-model="password"
+                required
+                placeholder="Enter password">
+              </b-form-input>
+            </b-form-group>
+
+            <b-button class="float-right" type="submit" variant="primary" data-dismiss="modal">Register</b-button>
+          </b-form>
+        </b-modal>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { REGISTER } from '@/stores/actions.type'
+
+export default {
+  name: 'Register',
+  data () {
+    return {
+      email: null,
+      username: null,
+      password: null
+    }
+  },
+  methods: {
+    onSubmit (email, username, password) {
+      this.$store
+        .dispatch(REGISTER, { email, username, password })
+        .then(() => this.$router.push({ name: 'dashboard' }))
+    }
+  }
+}
+</script>
