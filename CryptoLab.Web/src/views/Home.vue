b915a13edd24482be57e36272231e95424374ba5
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-11-05 20:42:44 +0100

    Updated Home component

--- CryptoLab.Web/src/views/Home.vue
+++ CryptoLab.Web/src/views/Home.vue
@@ -1,17 +1,42 @@
 <template>
-  <div class="home">
-    <Login msg="Welcome to Your Vue.js App"/>
+  <div class="text-center">
+    <br /><br /><br /><br />
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <h1 class="cover-heading">CryptoLab</h1>
+      <p class="lead">Make money & have fun</p>
+        <Login/> 
+        <h4>/</h4> 
+        <Register/>
+    </div>   
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import Login from '@/components/Login.vue'
+import Register from '@/components/Register.vue'
 
 export default {
   name: 'home',
   components: {
-    Login
+    Login,
+    Register
   }
 }
 </script>
+
+<style>
+a,
+a:focus,
+a:hover {
+  color: #fff;
+}
+
+.btn-secondary,
+.btn-secondary:hover,
+.btn-secondary:focus {
+  color: #333;
+  text-shadow: none; 
+  background-color: #fff;
+  border: .05rem solid #fff;
+}
+</style>
