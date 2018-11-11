1adb53ab53c55f890a0c26a30053c3b150a59446
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-11-11 14:25:30 +0100

    Added FastTransaction.vue

---
+++ CryptoLab.Web/src/components/FastTransaction.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-form v-on:submit.prevent="buy(amountOfMoney)">
+      <b-form-group id="exampleInputGroup1"
+        label="Buy coin:"
+        label-for="exampleInput1">
+        <b-form-input size="sm" id="exampleInput1"
+          v-model="amountOfMoney"
+          required
+          placeholder="value">
+        </b-form-input>
+      </b-form-group>
+      <b-button size="sm" class="float-right" type="submit" variant="primary">Buy</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+import { FAST_BUY_TRANSACTION } from '@/stores/actions.type'
+
+export default {
+  name: 'Wallet',
+  props: {
+    currency: String
+  },
+  data () {
+    return {
+      amountOfMoney: 0
+    }
+  },
+  methods: {
+    buy(amountOfMoney) {
+      this.$store
+        .dispatch(FAST_BUY_TRANSACTION, { currency: this.currency, amountOfMoney })
+    }
+  }
+}
+</script>
