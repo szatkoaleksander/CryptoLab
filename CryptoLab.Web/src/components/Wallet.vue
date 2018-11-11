af5d919aec8731be83a359491eb1e995581218d5
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-11-11 14:25:18 +0100

    Added Wallet.vue

---
+++ CryptoLab.Web/src/components/Wallet.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul id="example-1">
+      <li v-bind:key="item.id" v-for="item in userWallets">
+        {{ item.currency }} - {{ item.amountOfMoney }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { FETCH_WALLETS } from '@/stores/actions.type'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Wallet',
+  data () {
+    return {
+    }
+  },
+  created () {
+    this.$store.dispatch(FETCH_WALLETS)
+  },
+  computed: {
+    ...mapGetters([
+      'userWallets'
+    ])
+  }
+}
+</script>
