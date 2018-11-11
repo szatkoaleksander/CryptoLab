a9c1ff49153c81844fa5aa9723624966efdcab63
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-11-11 14:23:27 +0100

    Added new modules

--- CryptoLab.Web/src/stores/index.js
+++ CryptoLab.Web/src/stores/index.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import auth from './auth.module'
+import wallet from './wallet.module'
+import transaction from './transaction.module'
+import history from './history.module'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    auth
+    auth,
+    wallet,
+    transaction,
+    history
   }
 })
