fc3b553cbf0f350a87aa8aa76aed943dec7dc50b
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-11-13 20:06:21 +0100

    Added HistoryMarket

--- CryptoLab.Web/src/views/Market.vue
+++ CryptoLab.Web/src/views/Market.vue
@@ -1,26 +1,21 @@
 <template>
   <div>
-      <br/>
-      <b-container fluid>
-        <b-row class="shadow p-3 mb-3 bg-white rounded">
-          <b-col>
-            <TradingView :currency="this.currency" :key="$route.fullPath"/>
-          </b-col>
-        </b-row>
-        <b-row class="shadow p-3 mb-3 bg-white rounded">
-          <b-col>
-            <FastTransaction :currency="this.currency"/>
-          </b-col>
-          <b-col>
-            
-          </b-col>
+    <br/>
+    <b-container fluid>
+      <b-row class="shadow p-3 mb-3 bg-white rounded">
+        <b-col>
+          <TradingView :currency="this.currency" :key="$route.fullPath"/>
+        </b-col>
       </b-row>
-      </b-container>
+      <FastTransaction :currency="this.currency"/>
+      <HistoryMarket :currency="this.currency" :transactionType="1" :key="$route.fullPath"/>
+    </b-container>
   </div>
 </template>
 
 <script>
 import TradingView from '@/components/TradingView.vue'
+import HistoryMarket from '@/components/HistoryMarket.vue'
 import FastTransaction from '@/components/FastTransaction.vue'
 
 export default {
@@ -32,6 +27,7 @@ export default {
   },
   components: {
     TradingView,
+    HistoryMarket,
     FastTransaction
   },
   beforeRouteUpdate(to, from, next) {
