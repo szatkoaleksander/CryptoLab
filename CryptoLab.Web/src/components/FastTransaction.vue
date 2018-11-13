d65da7e314cebe5dde7e1206af9083e31d8fe8ad
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-11-13 20:06:41 +0100

    Added Sell option to FastTransaction

--- CryptoLab.Web/src/components/FastTransaction.vue
+++ CryptoLab.Web/src/components/FastTransaction.vue
@@ -1,22 +1,40 @@
 <template>
   <div>
-    <b-form v-on:submit.prevent="buy(amountOfMoney)">
-      <b-form-group id="exampleInputGroup1"
-        label="Buy coin:"
-        label-for="exampleInput1">
-        <b-form-input size="sm" id="exampleInput1"
-          v-model="amountOfMoney"
-          required
-          placeholder="value">
-        </b-form-input>
-      </b-form-group>
-      <b-button size="sm" class="float-right" type="submit" variant="primary">Buy</b-button>
-    </b-form>
+    <b-row class="shadow p-3 mb-3 bg-white rounded">
+      <b-col>
+      <b-form v-on:submit.prevent="buy(amountOfMoneyToBuy)">
+        <b-form-group id="exampleInputGroup1"
+          label="Buy coin:"
+          label-for="exampleInput1">
+          <b-form-input size="sm" id="exampleInput1"
+            v-model="amountOfMoneyToBuy"
+            required
+            placeholder="value">
+          </b-form-input>
+        </b-form-group>
+        <b-button size="sm" class="float-right" type="submit" variant="primary">Buy</b-button>
+      </b-form>
+      </b-col>
+      <b-col>
+      <b-form v-on:submit.prevent="sell(amountOfMoneyToSell)">
+        <b-form-group id="exampleInputGroup2"
+          label="Sell coin:"
+          label-for="exampleInput2">
+          <b-form-input size="sm" id="exampleInput2"
+            v-model="amountOfMoneyToSell"
+            required
+            placeholder="value">
+          </b-form-input>
+        </b-form-group>
+        <b-button size="sm" class="float-right" type="submit" variant="primary">Sell</b-button>
+      </b-form>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
-import { FAST_BUY_TRANSACTION } from '@/stores/actions.type'
+import { FAST_BUY_TRANSACTION, FAST_SELL_TRANSACTION } from '@/stores/actions.type'
 
 export default {
   name: 'Wallet',
@@ -25,13 +43,18 @@ export default {
   },
   data () {
     return {
-      amountOfMoney: 0
+      amountOfMoneyToBuy: 0,
+      amountOfMoneyToSell: 0
     }
   },
   methods: {
-    buy(amountOfMoney) {
+    buy (amountOfMoneyToBuy) {
       this.$store
-        .dispatch(FAST_BUY_TRANSACTION, { currency: this.currency, amountOfMoney })
+        .dispatch(FAST_BUY_TRANSACTION, { currency: this.currency, amountOfMoneyToBuy })
+    },
+    sell (amountOfMoneyToSell) {
+      this.$store
+        .dispatch(FAST_SELL_TRANSACTION, { currency: this.currency, amountOfMoneyToSell })
     }
   }
 }
