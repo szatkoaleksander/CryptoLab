b4cf06c2ed1d0dec50672a5c96dbab0e52ad3430
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-11-13 20:05:32 +0100

    Added history.module

---
+++ CryptoLab.Web/src/stores/history.module.js
@@ -0,0 +1,69 @@
+import axios from 'axios'
+import { HubConnectionBuilder, LogLevel } from '@aspnet/signalr'
+import JwtService from '@/common/jwt.service'
+import { API_URL } from '@/common/config'
+import { FETCH_MARKET_HISTORIES_BUY, FETCH_MARKET_HISTORIES_SELL } from './actions.type'
+import { SET_MARKET_HISTORIES_BUY, SET_MARKET_HISTORIES_SELL } from './mutations.type'
+
+const hubConnection = new HubConnectionBuilder()
+  .withUrl(API_URL + 'hub/histories', { accessTokenFactory: () => 'Bearer ' + JwtService.getToken() })
+  .configureLogging(LogLevel.Information)
+  .build()
+hubConnection.start()
+
+const state = {
+  marketHistoriesBuy: [],
+  marketHistoriesSell: []
+}
+
+const getters = {
+  marketHistoriesBuy (state) {
+    return state.marketHistoriesBuy
+  },
+  marketHistoriesSell (state) {
+    return state.marketHistoriesSell
+  }
+}
+
+const actions = {
+  [FETCH_MARKET_HISTORIES_BUY] (context, payload) {
+    axios.get(API_URL + 'histories/' + payload.currency + `/` + '0', { headers: { Authorization: 'Bearer ' + JwtService.getToken() } })
+      .then(({ data }) => {
+        context.commit(SET_MARKET_HISTORIES_BUY, data)
+      })
+  },
+  [FETCH_MARKET_HISTORIES_SELL] (context, payload) {
+    axios.get(API_URL + 'histories/' + payload.currency + `/` + '1', { headers: { Authorization: 'Bearer ' + JwtService.getToken() } })
+      .then(({ data }) => {
+        context.commit(SET_MARKET_HISTORIES_SELL, data)
+      })
+  }
+}
+
+const mutations = {
+  [SET_MARKET_HISTORIES_BUY] (state, marketHistoriesBuy) {
+    hubConnection.on('Add', (history) => {
+      if (history.operationType === 0) {
+        marketHistoriesBuy.push(history)
+      }
+    })
+
+    state.marketHistoriesBuy = marketHistoriesBuy
+  },
+  [SET_MARKET_HISTORIES_SELL] (state, marketHistoriesSell) {
+    hubConnection.on('Add', (history) => {
+      if (history.operationType === 1) {
+        marketHistoriesSell.push(history)
+      }
+    })
+
+    state.marketHistoriesSell = marketHistoriesSell
+  }
+}
+
+export default {
+  state,
+  actions,
+  mutations,
+  getters
+}
