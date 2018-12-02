b333fab20723a0d1cf4f94d737bce80c23e9c9ad
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-12-02 21:32:39 +0100

    Fixed test

--- CryptoLab.Test/Services/UserServiceTests.cs
+++ CryptoLab.Test/Services/UserServiceTests.cs
@@ -6,6 +6,7 @@ using CryptoLab.Infrastructure.Services;
 using Moq;
 using Xunit;
 using System.Linq;
+using AutoMapper;
 
 namespace CryptoLab.Test.Services
 {
@@ -16,10 +17,12 @@ namespace CryptoLab.Test.Services
         {
             var userRepositoryMock = new Mock<IUserRepository>();
             var encrypterMock = new Mock<IEncrypter>();
+            var mapper = new Mock<IMapper>();
+
             encrypterMock.Setup(x => x.GetSalt(It.IsAny<string>())).Returns("hash_hash_hash");
             encrypterMock.Setup(x => x.GetHash(It.IsAny<string>(),It.IsAny<string>())).Returns("salt_salt_salt"); 
 
-            var userService = new UserService(userRepositoryMock.Object, encrypterMock.Object);
+            var userService = new UserService(userRepositoryMock.Object, encrypterMock.Object, mapper.Object);
             await userService.RegisterAsync("test@test.com", "username1234", "hash");
 
             userRepositoryMock.Verify(x => x.AddAsync(It.IsAny<User>()), Times.Once);
