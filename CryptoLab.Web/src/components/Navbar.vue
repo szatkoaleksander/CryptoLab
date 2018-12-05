e271109fd9412fd9f15afd5c0a7874b91769e2b4
Aleksander Szatko <szatko.aleksander@gmail.com>
2018-12-05 19:10:12 +0100

    Added Menu, Footer and Navbar

---
+++ CryptoLab.Web/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-navbar toggleable type="light" variant="light">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#">
+      <router-link to="/app">CryptoLab</router-link>
+    </b-navbar-brand>
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav class="ml-auto" left>
+        <b-nav-text>
+
+        </b-nav-text>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto" right>
+        <b-nav-text>
+          
+        </b-nav-text>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar> 
+</template>
